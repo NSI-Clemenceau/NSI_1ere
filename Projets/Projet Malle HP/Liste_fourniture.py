5464e8b508bb07bc1537f5df95b4e2ecf32a733a
David Landry <david.landry@ac-nantes.fr>
2021-02-09 18:48:51 +0100

    Equilibre des poids pour résultats plus intéressants

--- Projets/Projet Malle HP/Liste_fourniture.py
+++ Projets/Projet Malle HP/Liste_fourniture.py
@@ -8,15 +8,16 @@ Liste de fournitures scolaires
 Auteur : David Landry
 '''
 
-fournitures_scolaires = [{'Nom' : 'Manuel scolaire', 'Poids' : 0.6, 'Mana' : 11},
-                         {'Nom' : 'Baguette magique', 'Poids' : 0.085, 'Mana' : 120},
-                         {'Nom' : 'Chaudron', 'Poids' : 2.7, 'Mana' : 2},
-                         {'Nom' : 'Boîte de fioles', 'Poids' : 1.2, 'Mana' : 4},
-                         {'Nom' : 'Téléscope', 'Poids' : 1.9, 'Mana' : 6},
-                         {'Nom' : 'Balance de cuivre', 'Poids' : 1.2, 'Mana' : 3},
-                         {'Nom' : 'Robe de travail', 'Poids' : 0.5, 'Mana' : 8},
-                         {'Nom' : 'Chapeau pointu', 'Poids' : 0.7, 'Mana' : 9},
-                         {'Nom' : 'Gants', 'Poids' : 0.6, 'Mana' : 25},
-                         {'Nom' : 'Cape', 'Poids' : 1.1, 'Mana' : 13}]
+fournitures_scolaires = \
+[{'Nom' : 'Manuel scolaire', 'Poids' : 0.55, 'Mana' : 11},
+{'Nom' : 'Baguette magique', 'Poids' : 0.085, 'Mana' : 120},
+{'Nom' : 'Chaudron', 'Poids' : 2.5, 'Mana' : 2},
+{'Nom' : 'Boîte de fioles', 'Poids' : 1.2, 'Mana' : 4},
+{'Nom' : 'Téléscope', 'Poids' : 1.9, 'Mana' : 6},
+{'Nom' : 'Balance de cuivre', 'Poids' : 1.3, 'Mana' : 3},
+{'Nom' : 'Robe de travail', 'Poids' : 0.5, 'Mana' : 8},
+{'Nom' : 'Chapeau pointu', 'Poids' : 0.7, 'Mana' : 9},
+{'Nom' : 'Gants', 'Poids' : 0.6, 'Mana' : 25},
+{'Nom' : 'Cape', 'Poids' : 1.1, 'Mana' : 13}]
 
 poids_maximal = 4
